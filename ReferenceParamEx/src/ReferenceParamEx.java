473a9686db9f9c0b5888242c63b392e36d34954b
DistincTao <142079919+DistincTao@users.noreply.github.com>
2023-11-14 16:32:50 +0900

    11/14

--- ReferenceParamEx/src/ReferenceParamEx.java
+++ ReferenceParamEx/src/ReferenceParamEx.java
@@ -7,8 +7,7 @@ public class ReferenceParamEx {
 		System.out.println("x : " + x[0]);
 		
 		change(x); // 참조타입을 매개변수로 전달
-		System.out.println("change 메소드 호출 후 x : " + x[0]);
-		
+		System.out.println("change 메소드 호출 후 x : " + x[0]);		
 	}
 
 	static void change (int[] x) {
