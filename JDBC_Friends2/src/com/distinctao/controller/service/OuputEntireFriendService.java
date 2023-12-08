9cafe0de38f382095024bee475da10ed6b7d6436
DistincTao <142079919+DistincTao@users.noreply.github.com>
2023-12-08 19:59:04 +0900

    12/8 -2

---
+++ JDBC_Friends2/src/com/distinctao/controller/service/OuputEntireFriendService.java
@@ -0,0 +1,22 @@
+package com.distinctao.controller.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.distinctao.dao.FriendMngDao;
+import com.distinctao.dao.FriendMngDaoImpl;
+import com.distinctao.service.FriendManagementService;
+import com.distinctao.vo.FriendVo;
+
+public class OuputEntireFriendService implements FriendManagementService {
+
+	@Override
+	public void toDo() throws ClassNotFoundException, SQLException {
+		FriendMngDao fmd = FriendMngDaoImpl.getInstance();
+		List<FriendVo> list = fmd.selectAllFriends();
+		
+		for (FriendVo f : list) {
+			System.out.println(f.toString());
+		}
+	}
+}
